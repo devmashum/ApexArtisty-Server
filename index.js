bcd6041654029e7182e179e5b9d9a98a0d2aaa78
devmashum <dev.mashum@gmail.com>
2023-11-28 01:21:30 +0100

    successfully add data to the carts as a creator

--- index.js
+++ index.js
@@ -31,10 +31,20 @@ async function run() {
 
         const artsCollection = client.db('ApexArtistry').collection('arts');
         const usersCollection = client.db('ApexArtistry').collection('users');
+        const cartCollection = client.db('ApexArtistry').collection('cart');
+        const contactCollection = client.db('ApexArtistry').collection('contact');
+        const contestCollection = client.db('ApexArtistry').collection('contest');
 
         app.get('/arts', async (req, res) => {
             const cursor = artsCollection.find();
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        // add contest from the creator 
+        app.post('/arts', async (req, res) => {
+            const newContest = req.body;
+            const result = await artsCollection.insertOne(newContest);
             res.send(result)
         })
 
@@ -49,16 +59,17 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
         // Middleware /Verify token 
         const verifyToken = (req, res, next) => {
             // console.log('inside verify token', req.headers);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'forbidden access' })
+                return res.status(401).send({ message: 'unauthorized access' })
             }
             const token = req.headers.authorization.split(' ')[1];
             jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: 'forbidden access' })
+                    return res.status(401).send({ message: 'unauthorized access' })
                 }
                 req.decoded = decoded;
                 next()
@@ -66,6 +77,86 @@ async function run() {
 
             // next()
         }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next()
+        }
+
+        // Verify admin with email
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin })
+        })
+        // use verify Creator after verifyToken
+        const verifyCreator = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const isCreator = user?.role1 === 'creator';
+            if (!isCreator) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next()
+        }
+
+        // Verify admin with email
+        app.get('/users/creator/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let creator = false;
+            if (user) {
+                creator = user?.role1 === 'creator';
+            }
+            res.send({ creator })
+        })
+
+        // Add to cart 
+        app.post('/cart', async (req, res) => {
+            const newItem = req.body;
+            const result = await cartCollection.insertOne(newItem);
+            res.send(result)
+        })
+        // get data from the cart
+        app.get('/cart', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+        // get cartId from cart Data
+        app.get('/cart', async (req, res) => {
+            const result = await cartCollection.find().toArray();
+            res.send(result);
+        })
+
+        // Delete Item from Cart
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
         // Load Users from the database
         app.get('/users', verifyToken, async (req, res) => {
             // console.log(req.headers);
@@ -80,7 +171,7 @@ async function run() {
             res.send(result);
         })
         // Update a user Role
-        app.patch('/users/admin/:id', async (req, res) => {
+        app.patch('/users/admin/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
             const updatedDoc = {
@@ -91,7 +182,20 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updatedDoc);
             res.send(result);
         })
+        // update a user to creator
 
+        app.patch('/users/creator/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role1: 'creator'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+        // 
         app.get('/arts/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
@@ -106,6 +210,19 @@ async function run() {
                 expiresIn: '1h'
             });
             res.send({ token })
+        })
+
+        // Contact Details send to DataBase
+        app.post('/contact', async (req, res) => {
+            const newContact = req.body;
+            const result = await contactCollection.insertOne(newContact);
+            res.send(result)
+        })
+        // Add all Contest to the database
+        app.post('/contest', async (req, res) => {
+            const newContest = req.body;
+            const result = await contestCollection.insertOne(newContest);
+            res.send(result)
         })
 
 
